867b83efc7a5445417e3413eb470cc1a6d369942
DrummD <drummd@rocketmail.com>
2022-10-19 20:03:15 -0700

    questions

--- Public/js/signup.js
+++ Public/js/signup.js
@@ -1,15 +1,17 @@
 async function signupFormHandler(event) {
     event.preventDefault();
     
-    const username = document.querySelector('#username-signup').value.trim();
+    const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if(username && email && password) {
+    console.table({ name, email, password });
+
+    if(name && email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({
-                username, email, password
+                name, email, password
             }),
             headers: {'Content-Type': 'application/json'}
         });
