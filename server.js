4a81b115b9ba694f7b99e9c4866de7405c930af2
DrummD <drummd@rocketmail.com>
2022-10-24 22:09:15 -0700

    hello

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./control');
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./control'));
+app.use(require('./controllers'));
 
 sequelize.sync({force:false}).then(() => {
     app.listen(PORT, () => console.log('Now listening http://localhost:3001/'));
